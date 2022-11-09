6779c85c6c59364b7f575093b6c663b44a4a4d2b
Munch <lilmunchinator@gmail.com>
2022-11-08 20:56:24 -0600

    saving property Assignment

---
+++ Assignments/propertychange.py
@@ -0,0 +1,25 @@
+# assignment
+# display 3 properties
+# allow the user to change one of the properties using the key
+# system will generate a random property and display all 3 properties again
+
+''' initial properties will be randomly generated
+1 Slash 905
+2 Fire damage 674
+3 poison affect 67
+
+Which property would you like to change?
+
+1 Speed attack 900
+2 Protection 1068
+3 
+
+loop until user is happy
+'''
+
+'''
+git commands to save files
+1. make sure you are in root python folder
+2. git status - tell us about any changes in our dictionary
+3. git add [file] - add file to our list of items to be connected
+'''
