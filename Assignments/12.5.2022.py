f4683cba62f6d1da1c6a4e5fee1a61b5fd14762e
Munch <lilmunchinator@gmail.com>
2022-12-06 20:33:23 -0600

    saving work

---
+++ Assignments/12.5.2022.py
@@ -0,0 +1,44 @@
+class Total:    # class name
+    # the following items are the properties of the class
+    Iron = 0
+    Emerald = 0
+    Copper = 0
+    Amethyst = 0
+    Gold = 0
+    Coal = 0
+    Flint = 0
+    Diamonds = 'y'
+    Crystal = 'n'
+
+    # Class constructor - this is assigns the given variables to the 'properties' of our object
+    def __init__(this, iron, emerald, copper, amethyst, gold, coal, flint, diamonds = 'y', crystal = 'n'):
+        this.Iron= iron
+        this.Emerald = emerald
+        this.Copper = copper
+        this.Amethyst = amethyst
+        this.Gold = gold
+        this.Coal = coal
+        this.Flint = flint
+        this.Diamonds = diamonds
+        this.Crystal = crystal
+
+    # a function that the house class can do
+    def UpdateTotal(this, iron, emerald, copper, amethyst, gold, coal, flint, diamonds, crystal):
+        # Save these values to the database
+        print("saving to the database")
+
+donaldsMinerals = Total( 56, 20, 78, 40, 36, 190, 45, 'y', 'n')
+#print("Bolis house is: ", bolisHouse.SquareFootage, "sqFt")
+
+isasMinerals = Total(90, 123, 74, 80, 24, 45, 145, 'n', 'n')           # Here we are creating instances of the class
+edwardsMinerals = Total( 87, 3, 47, 58, 46, 127, 90, 'y', 'n')
+gabrielsminerals = Total(129, 56, 23, 45, 45, 57, 67, 'y', 'y')
+laxsminerals = Total (67, 34, 87, 90, 65, 45, 78, 'n', 'y')
+
+members = [donaldsMinerals, isasMinerals, edwardsMinerals, gabrielsminerals, laxsminerals] # adding them to a list
+
+for member in members:        # looping through our list of house objects
+    if member.Iron > 40:
+        print(member.Iron, "You may keep some of your minerals!")
+    if member.Copper < 50:
+        print(member.Copper, 'You have earned a box of minerals!')
--- Assignments/csvGenerator.py
+++ Assignments/csvGenerator.py
@@ -9,7 +9,9 @@ def getRandomNumber():
     randomNumber = randint(1,1000)
     return str(randomNumber)
 def getRandomName():
-    names = ["Mario", "George", "Sheldon", "Edward", "Donald", "Oliva", "Alice", "Amy", "Fred", "Leo", "Rick", "Mark", "Walter", "Susan", "Hanna", "Jack", "Isa", "John", "Penny", "Stacy", "Sam", "Dean", "Nicole", "Tom", "Jerry"]
+    names = ["Mario", "George", "Sheldon", "Edward", "Donald", "Oliva", "Alice",
+     "Amy", "Fred", "Leo", "Rick", "Mark", "Walter", "Susan", "Hanna", "Jack",
+      "Isa", "John", "Penny", "Stacy", "Sam", "Dean", "Nicole", "Tom", "Jerry"]
     return choice(names)
 
 
@@ -23,5 +25,7 @@ while (x < 25):
     #print(getRandomNumber())
     #print("John", ",", "56", ",", "47", ",", "190", ",", "176", ",", "1000")
     f = open("test.csv", "a")
-    f.write(getRandomName() + "," + getRandomNumber() + "," + getRandomNumber() + "," + getRandomNumber() + "," + getRandomNumber() + "," + getRandomNumber() + "\n")
+    f.write(getRandomName() + "," + getRandomNumber() + "," + getRandomNumber() +
+     "," + getRandomNumber() + "," + getRandomNumber() + "," +
+      getRandomNumber() + "\n")
     f.close()
